49eb28212ca13232f76ee9c9651078a8873a2a56
kschallenberger <karol.schallenberger@git.cwi.com.br>
2023-03-07 17:50:02 -0300

    criando projeto

---
+++ api/src/main/java/br/com/cwi/segurancaaula01/SegurancaAula01Application.java
@@ -0,0 +1,13 @@
+package br.com.cwi.segurancaaula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurancaAula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurancaAula01Application.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/cwi/segurancaaula01/SegurancaAula01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cwi.segurancaaula01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurancaAula01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
